c43a7333b4bf3f2c9a9f9082ac61de40f2b5d19f
SimpNick6703 <harshluha05012003@gmail.com>
2025-11-11 21:14:44 +0530

    Update Netlify build

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
-# Netlify build script - frontend only
+#!/usr/bin/env bash
+
+# Install Rust toolchain so pydantic-core can compile if no prebuilt wheel exists
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
 echo "Installing Node.js dependencies..."
 npm install
 
